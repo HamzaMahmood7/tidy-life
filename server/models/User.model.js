7ef5fc3d31672bde55db1304079c0717b80351e3
Hamza Mahmood <hamzamahmood195@gmail.com>
2026-01-22 00:04:22 +0000

    created the auth routes for user signup and login. install bcryptjs for password encryption. Updated user model. changed the local host port number to match 5005.

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -14,10 +14,14 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Password is required.']
     },
-    name: { 
+    username: { 
       type: String,
-      required: [true, 'Name is required.']
-    },    
+      required: [true, 'Username is required.'],
+      unique: true
+    }, 
+    profilePicture: {
+      type: String,
+    }   
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
