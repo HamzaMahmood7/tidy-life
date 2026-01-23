5edd05dbf5b6bf910456ad8775ab708065ae4e64
Hamza Mahmood <hamzamahmood195@gmail.com>
2026-01-23 15:32:49 +0000

    added some initial css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
-import UserProfilePage from './pages/UserProfilePage'
-import Footer from './components/Footer'
-import NotFoundPage from './pages/NotFoundPage'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import Footer from "./components/Footer";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-
   return (
     <>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage /> } />
-        <Route path='/profile' element={<UserProfilePage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
